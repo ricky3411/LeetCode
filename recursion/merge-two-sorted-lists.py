c0c216336f44e1fbe196991dc5aa2c9309edfa36
ricky3411 <78570667+ricky3411@users.noreply.github.com>
2025-02-25 01:39:09 -0500

    LC-merge-two-sorted-lists [Runtime: 0 ms; Memory: 17.9 MB]

--- recursion/merge-two-sorted-lists.py
+++ recursion/merge-two-sorted-lists.py
@@ -7,22 +7,22 @@ class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
 
         dummy = ListNode()
-        curr = dummy 
+        curr = dummy
 
         while list1 and list2:
             if list1.val <= list2.val:
                 curr.next = list1
                 curr = list1
                 list1 = list1.next
-            else: 
+            else:
                 curr.next = list2
                 curr = list2
                 list2 = list2.next
 
-
-        if list1 and list2 is None:
+        if not list1:
+            curr.next = list2
+        else:
             curr.next = list1
-        if list2 and list1 is None:
-            curr.next = list2 
 
-        return dummy.next 
+        return dummy.next
+        
