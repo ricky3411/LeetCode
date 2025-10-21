8423f0cea9c02f476faaeefa5d1aa6f2cc6327eb
ricky3411 <78570667+ricky3411@users.noreply.github.com>
2025-10-21 01:09:26 -0400

    LC-two-sum [Runtime: 4 ms; Memory: 19 MB]

--- array/two-sum.py
+++ array/two-sum.py
@@ -1,15 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
-        newList = []  # This will store pairs of (value, index)
+        ans = {}
 
-        for idx, val in enumerate(nums):
-            diff = target - val  # Calculate the difference
+        for idx, num in enumerate(nums):
+            
+            diff = target - num
 
-            # Check if the difference exists in the stored list
-            for stored_val, stored_idx in newList:
-                if stored_val == diff:
-                    return [stored_idx, idx]  # Return the indices of the pair
-
-            # Append the current value and its index to the list
-            newList.append((val, idx))
+            if diff in ans:
+                return [idx, ans[diff]]
+            else:
+                ans[num] = idx
